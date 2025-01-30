b1fb1ad2e297f7354d675278f39ca30da0ed2684
zadmin <zadmin@vm-lab72-7-430.asia-south1-b.c.niit-stackroute-2.internal>
2025-01-30 15:52:25 +0530

    code

---
+++ springCloudRunApplication/src/main/java/com/example/springCloudRunApplication/SpringCloudRunApplication.java
@@ -0,0 +1,13 @@
+package com.example.springCloudRunApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudRunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudRunApplication.class, args);
+	}
+
+}
---
+++ springCloudRunApplication/src/test/java/com/example/springCloudRunApplication/SpringCloudRunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springCloudRunApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudRunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
